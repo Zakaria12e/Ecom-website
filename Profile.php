855e64002bd2c90629a57d2be37589a6412f9f3a
Zakaria12e <zakariael1212@gmail.com>
2023-12-14 22:32:20 +0000

    final v

--- Profile.php
+++ Profile.php
@@ -69,6 +69,7 @@ if (!isset($_SESSION['username'])) {
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="css/Profile.css">
     <link rel="stylesheet" href="css/nav.css">
     <link rel="icon" href="images/logogravey.jpg" type="image/png" sizes="16x16">
--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@
          <h3>contact info</h3>
          <a href="tel:+212643131183"><i class="fas fa-phone"></i>+212-643131183</a>
          <a href="tel:+212708076505"><i class="fas fa-phone"></i>+212-708076505</a>
-         <a href="mailto:example@example.com"><i class="fas fa-envelope"></i> elbidali.zakaria@gmail.com </a>
+         <a href="mailto:elidali.zakaria@gmail.com"><i class="fas fa-envelope"></i> elbidali.zakaria@gmail.com </a>
       </div>
 
       <div class="box">
